43a78e65460aa78feaad0038900bf89d84c6c3d0
redmond <5iFUu6SRAsAb0uS5>
2016-09-16 00:14:46 +0400

    working with router

--- resources/assets/vue/admin/pages/folder.vue
+++ resources/assets/vue/admin/pages/folder.vue
@@ -1,8 +1,62 @@
 <template>
-    fuck
+
+    <div class="top-line">
+        <div class="row">
+            <div class="col-sm-6">
+                <searchform></searchform>
+                <br/>
+                <h1>{{ $t('folders.files_and_folders') }}</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" id="folders">
+        <folder v-for="folder in folders" v-show="folder.path" :folder="folder" @deleted="onDeleted" @moved="onMoved" @prepared-for-update="onPreparedForUpdate"></folder>
+    </div>
+
+    <folderstree :folder="selectedFolder" :folders-tree="foldersTree" @hidden-bs-modal="onHiddenBsModal"></folderstree>
+
+
+    <folder-update-popup :folder="selectedFolderForUpdate"></folder-update-popup>
+
 
 </template>
 <script type="text/coffeescript" lang="coffee">
     Vue = require('vue')
-    module.exports = Vue.extend({})
+
+    SearchForm = require('_admin/components/search_form')
+    FoldersTree = require('_admin/components/folders_tree')
+    Folder = require('_admin/components/folder')
+    FolderUpdatePopup = require('_admin/components/folder_update_popup')
+
+    module.exports = Vue.extend
+        created: ->
+            this.getFolderAndFiles()
+            this.$route.params.id # понеслось
+        data: ->
+            folders: []
+            selectedFolder: null,
+            selectedFolderForUpdate: null
+            foldersTree: null,
+        components:
+            searchform: SearchForm
+            folderstree: FoldersTree
+            folder: Folder
+            'folder-update-popup': FolderUpdatePopup
+        methods:
+            getFolderAndFiles: ->
+                this.$http.get('/api').then (response) =>
+                    this.folders = response.data
+            onDeleted: (folder) ->
+                this.folders.$remove(folder)
+            onMoved: (folder) ->
+                this.selectedFolder = folder
+                this.$http.get('/api/tree').then (response) =>
+                    this.foldersTree = response.data
+            onHiddenBsModal: ->
+                this.foldersTree = null
+                this.selectedFolder = null
+
+            onPreparedForUpdate: (folder) ->
+                this.selectedFolderForUpdate = folder
 </script>
