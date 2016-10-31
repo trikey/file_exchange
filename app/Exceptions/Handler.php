db34594f793a570e1b9403162bc411169f021849
trikey <belitskii@gmail.com>
2016-10-31 13:34:20 +0300

    add redirect to auth if session expired

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ( $e instanceof \Illuminate\Session\TokenMismatchException ) {
+            return redirect('/login');
+        }
         if($this->isHttpException($e)){
             switch ($e->getStatusCode()) {
                 case '404':
